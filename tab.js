52c018e4f4e612a37da66adda3ca4357490f7f0b
superiorz1 <orckang@gmail.com>
2021-12-17 03:17:44 +0900

    2021.12.17. 03.17

--- tab.js
+++ tab.js
@@ -1,8 +1,26 @@
-for (let i = 0; i < $('.tab-button').length; i++) {
-    $('.tab-button').eq(i).click(function() {
-        $('.tab-button').removeClass('active');
-        $('.tab-content').removeClass('show');
-        $('.tab-button').eq(i).addClass('active');
-        $('.tab-content').eq(i).addClass('show');
-    });
+// for (let i = 0; i < $('.tab-button').length; i++) {
+//     $('.tab-button').eq(i).click(function() {
+//         openTab(i);
+//     });
+// }
+
+$('.list').click(function(e) {
+    //탭열기(내가 누른 버튼에 숨겨진 숫자)
+    openTab(e.target.dataset.id);
+});
+
+function openTab(num) {
+    $('.tab-button').removeClass('active');
+    $('.tab-content').removeClass('show');
+    $('.tab-button').eq(num).addClass('active');
+    $('.tab-content').eq(num).addClass('show');
 }
+var obj = {brand: 'BMW', model: 520};
+
+// document.getElementsByClassName('tab-content')[0].querySelector('h4').innerText = obj.brand;
+// document.getElementsByClassName('tab-content')[0].querySelectorAll('p')[1].innerText = obj.model;
+
+var data = [{brand: 'BMW'}, {model: 520}];
+
+$('#title').html(data[0].brand);
+$('#text').html(data[1].model);
